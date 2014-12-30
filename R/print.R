80d1d0a8fb521a4f216fd4e0f47c01e0c9edf8b1
hafen <rhafen@gmail.com>
2014-12-30 10:12:34 -0800

    Add knit_print method for more natural embedding in R Markdown

--- R/print.R
+++ R/print.R
@@ -77,3 +77,29 @@ plot.BokehFigure <- function(x, y, ...) {
     )
   }
 }
+
+# Reusable function for registering a set of methods with S3 manually. The
+# methods argument is a list of character vectors, each of which has the form
+# c(package, genname, class).
+registerMethods <- function(methods) {
+  lapply(methods, function(method) {
+    pkg <- method[[1]]
+    generic <- method[[2]]
+    class <- method[[3]]
+    func <- get(paste(generic, class, sep="."))
+    if (pkg %in% loadedNamespaces()) {
+      registerS3method(generic, class, func, envir = asNamespace(pkg))
+    }
+    setHook(
+      packageEvent(pkg, "onLoad"),
+      function(...) {
+        registerS3method(generic, class, func, envir = asNamespace(pkg))
+      }
+    )
+  })
+}
+
+knit_print.BokehFigure <- function(x, ..., options = NULL) {
+  knitr::knit_print(htmlwidgets:::toHTML(plot(x), standalone = FALSE, knitrOptions = options), options = options,  ...)
+}
+
--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,7 @@
 .onLoad <- function(libname, pkgname){
+  registerMethods(list(
+    c("knitr", "knit_print", "BokehFigure")
+  ))
+
   options(bokeh_theme = tableau_theme)
 }
