6f27ee1e16570722cfe73771ed06fac9e9af8a3b
jrowen <jonathanro@gmail.com>
2015-05-19 02:51:44 +0000

    Added callback

--- R/fig_url.R
+++ R/fig_url.R
@@ -18,6 +18,22 @@ add_tap_url <- function(fig, url, renderer_ref) {
   fig
 }
 
+add_tap_callback <- function(fig, callback, renderer_ref) {
+  
+  u_id <- gen_id(fig, c(renderer_ref$id, "callback"))
+  act <- callback_model(u_id, callback)
+  
+  id <- gen_id(fig, c(renderer_ref$id, "TapTool"))
+  tap <- tap_model(id, fig$x$spec$ref, renderer_ref, act$ref)
+  
+  fig$x$spec$model$plot$attributes$tools[[id]] <- tap$ref
+  fig$x$spec$model[[id]] <- tap$model
+  
+  fig$x$spec$model[[u_id]] <- act$model
+  
+  fig
+}
+
 tap_model <- function(id, plot_ref, renderer_ref, action_ref) {
   res <- base_model_object("TapTool", id)
   res$model$attributes$plot <- plot_ref
@@ -31,5 +47,13 @@ open_url_model <- function(id, url) {
   res <- base_model_object("OpenURL", id)
   res$model$attributes$url <- url
 
+  res
+}
+
+callback_model <- function(id, callback) {
+  res <- base_model_object("Callback", id)
+  res$model$attributes$args <- list()
+  res$model$attributes$code <- callback
+  
   res
 }
