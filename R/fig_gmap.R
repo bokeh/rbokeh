43d9e439e4619d0101143cd43f439702c80cdf05
hafen <rhafen@gmail.com>
2015-07-08 23:13:01 -0400

    Updates to help with check

--- R/fig_gmap.R
+++ R/fig_gmap.R
@@ -11,6 +11,7 @@
 #' @param lng longitude where the map should be centered
 #' @param zoom initial \href{https://developers.google.com/maps/documentation/staticmaps/\#Zoomlevels}{zoom level} to use when displaying the map
 #' @param map_type \href{https://developers.google.com/maps/documentation/staticmaps/\#MapTypes}{map type} to use for the plot - one of "hybrid", "satellite", "roadmap", "terrain"
+#' @param map_style a json string of a Google Maps style - see \code{\link{gmap_style}}
 #' @template dots-figure
 #' @note This can be used in the same way as \code{\link{figure}}, adding layers on top of the Google Map.
 #' @inheritParams figure
