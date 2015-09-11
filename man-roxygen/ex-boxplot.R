b9e1d64ae6ee0c745c155d146e19afe56ac9face
hafen <rhafen@gmail.com>
2015-09-10 23:17:39 -0700

    Fix boxplot example

--- man-roxygen/ex-boxplot.R
+++ man-roxygen/ex-boxplot.R
@@ -1,6 +1,6 @@
 \donttest{
 figure(ylab = "Height (inches)", width = 600) %>%
-  ly_boxplot(voice.part, height, data = lattice::singer) %>%
+  ly_boxplot(voice.part, height, data = lattice::singer)
 
 # change orientation of x axis labels
 figure(ylab = "Height (inches)", width = 600) %>%
