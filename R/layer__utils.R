1e5e7bb82a0f388cdf6dd2d5803979d809495374
Barret Schloerke <schloerke@gmail.com>
2015-09-18 17:05:06 -0400

    function to grab values from unnamed list into named list

--- R/layer__utils.R
+++ R/layer__utils.R
@@ -1,2 +1,38 @@
 
 sb <- substitute
+
+#' @import stringr
+grab <- function(...) {
+  nameVals <- structure(as.list(match.call()[-1]), class = "uneval") %>%
+    unlist() %>%
+    as.character() %>%
+    lapply(function(arg) {
+      if (str_detect(arg, "^sb\\(") && str_detect(arg, "\\)$")) {
+        arg <- arg %>% str_replace("^sb\\(", "") %>% str_replace("\\)", "")
+      }
+      arg
+    }) %>% unlist()
+
+  argVals <- list(...)
+  names(argVals) <- nameVals
+
+  if(length(argVals$dots) > 1) {
+    dots <- argVals$dots[-1]
+    dotVals <- as.character(dots)
+    dotNames <- names(dots)
+    for (i in seq_along(dots)) {
+      argVals[[dotNames[i]]] <- dotVals[i]
+    }
+  }
+  argVals$dots <- NULL
+
+  # print(argVals)
+  # eval(deparse(args))
+  # args[[1]] <- eval(args[[1]])
+  # list(
+  #   args,
+  #   match.call(),
+  #   sys.calls()
+  # )
+  argVals
+}
