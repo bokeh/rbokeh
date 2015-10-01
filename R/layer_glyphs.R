283c46d1321d8673709231cfdb248456ac4012ff
Barret Schloerke <schloerke@gmail.com>
2015-10-01 15:07:18 -0400

    annular wedge

--- R/layer_glyphs.R
+++ R/layer_glyphs.R
@@ -18,62 +18,65 @@
 #' @example man-roxygen/ex-annwedge.R
 #' @family layer functions
 #' @export
-ly_annular_wedge <- function(fig, x, y = NULL, data = NULL,
+ly_annular_wedge <- function(
+  fig, x, y = NULL, data = NULL,
   inner_radius = 0.1, outer_radius = 0.3,
   start_angle = 0, end_angle = 2*pi, direction = "anticlock",
   color = NULL, alpha = 1,
-  hover = NULL, url = NULL, legend = NULL, lname = NULL, lgroup = NULL, ...) {
+  hover = NULL, url = NULL, legend = NULL, lname = NULL, lgroup = NULL, ...
+) {
 
   validate_fig(fig, "ly_annular_wedge")
 
-  xname <- deparse(substitute(x))
-  yname <- deparse(substitute(y))
+  glyph = "annular_wedge"
+  dots <- substitute(list(...))
+  args <- sub_names(fig, data,
+    grab(
+      sb(x),
+      sb(y),
+      p_sb(glyph),
+      p_sb(inner_radius),
+      p_sb(outer_radius),
+      p_sb(start_angle),
+      p_sb(end_angle),
+      p_sb(direction),
+      p_sb(color),
+      p_sb(alpha),
+      sb(hover),
+      sb(url),
+      sb(legend),
+      sb(lname),
+      sb(lgroup),
+      dots
+    )
+  )
 
-  ## deal with possible named inputs from a data source
-  if(!is.null(data)) {
-    dots <- substitute(list(...))[-1]
-    args <- lapply(dots, function(x) v_eval(x, data))
-    x            <- v_eval(substitute(x), data)
-    y            <- v_eval(substitute(y), data)
-    color        <- v_eval(substitute(color), data)
-    inner_radius <- v_eval(substitute(inner_radius), data)
-    outer_radius <- v_eval(substitute(outer_radius), data)
-    start_angle  <- v_eval(substitute(start_angle), data)
-    end_angle    <- v_eval(substitute(end_angle), data)
-  } else {
-    args <- list(...)
-  }
-
-  hover <- get_hover(substitute(hover), data, parent.frame())
-  url <- get_url(url, data)
-  xy_names <- get_xy_names(x, y, xname, yname, args)
-  ## translate different x, y types to vectors
-  xy <- get_xy_data(x, y)
-  lgroup <- get_lgroup(lgroup, fig)
-
-  args <- c(args, list(glyph = "annular_wedge", color = color, alpha = alpha,
-    inner_radius = inner_radius, outer_radius = outer_radius,
-    start_angle = start_angle, end_angle = end_angle,
-    direction = direction))
+  # args <- c(args, list(glyph = "annular_wedge", color = color, alpha = alpha,
+  #   inner_radius = inner_radius, outer_radius = outer_radius,
+  #   start_angle = start_angle, end_angle = end_angle,
+  #   direction = direction))
   if(missing(alpha))
-    args$alpha <- NULL
+    args$params$alpha <- NULL
 
-  args <- resolve_color_alpha(args, has_line = TRUE, has_fill = TRUE, fig$x$spec$layers[[lgroup]], theme = fig$x$spec$theme)
+  args$params <- resolve_color_alpha(args$params, has_line = TRUE, has_fill = TRUE, fig$x$spec$layers[[args$lgroup]], theme = fig$x$spec$theme)
 
   ## see if any options won't be used and give a message
-  check_opts(args, "annular_wedge", names(formals(ly_annular_wedge)))
+  check_opts(args$params, "annular_wedge", names(formals(ly_annular_wedge)))
 
   check_arc_direction(direction)
 
-  axis_type_range <- get_glyph_axis_type_range(x, y, assert_x = "numeric", assert_y = "numeric")
+  axis_type_range <- get_glyph_axis_type_range(args$x, args$y, assert_x = "numeric", assert_y = "numeric")
 
   mc <- lapply(match.call(), deparse)
 
-  make_glyph(fig, type = "annular_wedge", lname = lname, lgroup = lgroup,
-    data = xy, data_sig = ifelse(is.null(data), NA, digest(data)),
-    args = args, axis_type_range = axis_type_range,
-    hover = hover, url = url, legend = legend,
-    xname = xy_names$x, yname = xy_names$y, ly_call = mc)
+  aa <<- args
+  print(args)
+
+  make_glyph(fig, type = "annular_wedge", lname = args$lname, lgroup = args$lgroup,
+    data = args[c("x", "y")], data_sig = ifelse(is.null(data), NA, digest(data)),
+    args = args$params, axis_type_range = axis_type_range,
+    hover = args$hover, url = args$url, legend = args$legend,
+    xname = args$xName, yname = args$yName, ly_call = mc)
 }
 
 #' Add an "annulus" layer to a Bokeh figure
--- man-roxygen/ex-barret.R
+++ man-roxygen/ex-barret.R
@@ -10,3 +10,6 @@ load_all(); figure() %>% ly_points(Sepal.Length, data = ir, color = glyphCol) ->
 load_all(); figure() %>% ly_points(Sepal.Length, data = ir, glyph = glyphVal, hover = Species) -> a; a
 load_all(); figure() %>% ly_points(Sepal.Length, data = ir, color = Species) -> a; a
 
+# ly_annular_wedge
+load_all(); figure() %>% ly_annular_wedge(Sepal.Length, Sepal.Width, data = iris, end_angle = rescale(Petal.Length)*2*pi, inner_radius = 0.1, outer_radius = 0.15, alpha = 0.5, hover = Species, color = Species) -> a; a
+
