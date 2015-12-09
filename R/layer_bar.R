811a2d2fc61c8a946e65b8165691a4a4621fc5e0
Barret Schloerke <schloerke@gmail.com>
2015-12-09 18:50:40 -0500

    pass better color value into ly_rect

--- R/layer_bar.R
+++ R/layer_bar.R
@@ -132,9 +132,11 @@ ly_bar <- function(
   res$x <- NULL
   res$y <- NULL
 
+  colorValue <- if (is.null(colorname)) args$params$color else colorname
   do.call(ly_rect, append(list(fig = fig,
     xleft = "xleft", ybottom = "ybottom", xright = "xright", ytop = "ytop",
     xlab = args$info$xName, ylab = args$info$yName,
-    color = colorname, data = res,
+    data = res,
+    color = colorValue,
     lname = args$info$lname, lgroup = args$info$lgroup, legend = args$info$legend), remainingArgs))
 }
