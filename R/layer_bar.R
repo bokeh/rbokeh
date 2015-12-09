4002c255fb6f8a2d11df28e05cb316b0530a2d60
Barret Schloerke <schloerke@gmail.com>
2015-12-09 18:50:25 -0500

    do not rename data.  x/ylab will pick up the correct name

--- R/layer_bar.R
+++ R/layer_bar.R
@@ -123,9 +123,6 @@ ly_bar <- function(
     names(res)[ind] <- colorname
   }
 
-  names(res)[which(names(res) == "xleft")] <- args$info$xName
-  names(res)[which(names(res) == "ybottom")] <- args$info$yName
-
   badParamNames = c("color", "origin","breaks","right","binwidth", "position")
   remainingArgs = args$params
   remainingArgs = remainingArgs[! (names(remainingArgs) %in% badParamNames)]
@@ -135,8 +132,8 @@ ly_bar <- function(
   res$x <- NULL
   res$y <- NULL
 
-  do.call(ly_rect, c(list(fig = fig,
-    xleft = args$info$xName, ybottom = args$info$yName, xright = "xright", ytop = "ytop",
+  do.call(ly_rect, append(list(fig = fig,
+    xleft = "xleft", ybottom = "ybottom", xright = "xright", ytop = "ytop",
     xlab = args$info$xName, ylab = args$info$yName,
     color = colorname, data = res,
     lname = args$info$lname, lgroup = args$info$lgroup, legend = args$info$legend), remainingArgs))
