031874f1685e1b1efdfe379ce310d2c9cbcbdbc9
hafen <rhafen@gmail.com>
2015-12-09 16:52:52 -0800

    Add catjitter function for jittering categorical scales

---
+++ R/misc.R
@@ -0,0 +1,11 @@
+
+#' Add a small amount of (rbokeh-compatible) noise to a character vector
+#'
+#' @param x numeric vector to which jitter should be added
+#' @param factor a factor between 0 and 1 that
+#' @export
+# add seealso and example
+catjitter <- function(x, factor = 0.8) {
+  # validate factor and x
+  paste0(x, ":", runif(min = 0.5 * (1 - factor), max = 0.5 + 0.5 * factor, nrow(dd)))
+}
