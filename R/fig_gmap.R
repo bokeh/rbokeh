978923d5114156ecc994f61d4f2b766203a22e0e
hafen <rhafen@gmail.com>
2016-02-29 16:16:22 -0800

    Fix gmap doc URLs

--- R/fig_gmap.R
+++ R/fig_gmap.R
@@ -9,8 +9,8 @@
 #'
 #' @param lat latitude where the map should be centered
 #' @param lng longitude where the map should be centered
-#' @param zoom initial \href{https://developers.google.com/maps/documentation/staticmaps/\#Zoomlevels}{zoom level} to use when displaying the map
-#' @param map_type \href{https://developers.google.com/maps/documentation/staticmaps/\#MapTypes}{map type} to use for the plot - one of "hybrid", "satellite", "roadmap", "terrain"
+#' @param zoom initial \href{https://developers.google.com/maps/documentation/staticmaps/#Zoomlevels}{zoom level} to use when displaying the map
+#' @param map_type \href{https://developers.google.com/maps/documentation/staticmaps/#MapTypes}{map type} to use for the plot - one of "hybrid", "satellite", "roadmap", "terrain"
 #' @param map_style a json string of a Google Maps style - see \code{\link{gmap_style}}
 #' @note This can be used in the same way as \code{\link{figure}}, adding layers on top of the Google Map.
 #' There is an open issue documenting points appearing to sometimes be a few pixels off from their intended location.
