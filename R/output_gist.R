159c027049a50efc7410c1a64b5f038f6470e0e3
hafen <rhafen@gmail.com>
2016-03-04 01:16:33 -0800

    Rename rbokeh2gist to widget2gist

--- R/output_gist.R
+++ R/output_gist.R
@@ -1,9 +1,9 @@
-#' Export rbokeh plot to a gist
+#' Export htmlwidget plot to a gist
 #'
-#' @param fig_str a string containing R code to create an rbokeh figure
+#' @param fig_str a string containing R code to create an htmlwidget
 #' @param name name of the gist
 #' @param created optional string for a "Created by" to preceed the README
-#' @param description text to go in README.md
+#' @param description optional text to go in README.md to describe the gist
 #' @param license license under which gist is released - one of those accepted here: \url{http://bl.ocks.org/licenses.txt}
 #' @param border should the bl.ocks.org iframe have a border?
 #' @param scrolling should the bl.ocks.org iframe scroll?
@@ -12,15 +12,15 @@
 #' @note
 #' This requires that you have a github personal access token stored as an environment variable \code{GITHUB_PAT}.  See \code{\link[gistr]{gist_create}} for more information.
 #'
-#' Also note that this currently can't handle thumbnails but will find a way to do that soon.
+#' Also note that this currently can't handle thumbnails but we are looking into ways to do that.
 #' @examples
 #' \dontrun{
-#' rbokeh2gist("figure() %>% ly_points(1:10)", name = "test")
+#' widget2gist("figure() %>% ly_points(1:10)", name = "test")
 #' }
 #' @importFrom gistr gist_create
 #' @importFrom utils browseURL
 #' @export
-rbokeh2gist <- function(fig_str, name,
+widget2gist <- function(fig_str, name,
   created = NULL, description = "",
   license = c("none", "apache-2.0", "bsd-2-clause", "bsd-3-clause", "cc-by-4.0", "cc-by-nc-4.0", "cc-by-nc-nd-4.0", "cc-by-nc-sa-4.0", "cc-by-nd-4.0", "cc-by-sa-4.0", "cddl-1.0", "epl-1.0", "gpl-2.0", "gpl-3.0", "lgpl-2.1", "lgpl-3.0", "mit", "mpl-2.0"),
   border = TRUE, scrolling = FALSE,
@@ -37,15 +37,20 @@ rbokeh2gist <- function(fig_str, name,
 
   p <- eval(parse(text = fig_str))
 
-  if(!inherits(p, "rbokeh"))
-    stop("Argument 'fig_str' must be a string specifying an rbokeh plot")
-
-  dir <- tempfile(pattern = "rbokeh_gist_", fileext = "")
+  dir <- tempfile(pattern = "widget_gist_", fileext = "")
   dir.create(dir)
 
-  rbokeh2html(p, file = file.path(dir, "index.html"), secure = secure)
-
-  # save_figure(p, file = file.path(dir, "thumbnail.png"))
+  if(inherits(p, "rbokeh")) {
+    rbokeh2html(p, file = file.path(dir, "index.html"), secure = secure)
+    # save_figure(p, file = file.path(dir, "thumbnail.png"))
+  } else if(inherits(p, "htmlwidget")) {
+    try_res <- try(htmlwidgets::saveWidget(p,
+      file = file.path(dir, "index.html"),
+      selfcontained = TRUE))
+    if(inherits(try_res, "error"))
+      stop("Widget could not be saved as a self contained file.")
+    unlink(file.path(dir, "index_files"), recursive = TRUE)
+  }
 
   readme <- NULL
   if(!is.null(created))
