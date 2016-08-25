f433e62eb3df25b708e4faff388e6de0a423aa37
hafen <rhafen@gmail.com>
2016-08-25 10:58:40 -0700

    Update ly_bar to include group information in hover and make hover FALSE by default

--- man-roxygen/ex-bar.R
+++ man-roxygen/ex-bar.R
@@ -6,16 +6,16 @@ figure() %>%
 
 # total yield per variety
 figure() %>%
-  ly_bar(variety, yield, data = lattice::barley) %>%
+  ly_bar(variety, yield, data = lattice::barley, hover = TRUE) %>%
   theme_axis("x", major_label_orientation = 90)
 
-# swap axes
+# swap axes and add hover
 figure() %>%
-  ly_bar(yield, variety, data = lattice::barley)
+  ly_bar(yield, variety, data = lattice::barley, hover = TRUE)
 
 # stack by year
 figure() %>%
-  ly_bar(variety, yield, color = year, data = lattice::barley) %>%
+  ly_bar(variety, yield, color = year, data = lattice::barley, hover = TRUE) %>%
   theme_axis("x", major_label_orientation = 90)
 
 # proportional bars
