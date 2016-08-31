f1b850fb873872dddafe1988d6a756d2ca044eb4
Steve Lianoglou <lianoglou.steve@gene.com>
2016-08-30 21:58:42 -0700

    back out les mis changes so that this is only in a branch

--- R/layer_shapes.R
+++ R/layer_shapes.R
@@ -118,7 +118,6 @@ ly_polygons <- function(
 #' @template par-legend
 #' @template par-lnamegroup
 #' @template dots-fillline
-#' @example man-roxygen/ex-rect.R
 #' @family layer functions
 #' @export
 ly_rect <- function(
