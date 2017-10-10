2f6038e8eb4c0eaf3b796d293d90a8be604695f2
hafen <rhafen@gmail.com>
2017-10-10 15:24:34 -0700

    Update rbokeh.js to use new htmlwidgets approach

--- inst/htmlwidgets/rbokeh.js
+++ inst/htmlwidgets/rbokeh.js
@@ -3,79 +3,75 @@ HTMLWidgets.widget({
 
   type: 'output',
 
-  initialize: function(el, width, height) {
-    return {
-      modelid: '',
-      elementid: '',
-      width: width,
-      height: height
-    };
-  },
-
-  renderValue: function(el, x, instance) {
+  factory: function(el, width, height) {
 
     //clear el for Shiny/dynamic contexts
     el.innerHTML = '';
+    var bokeh_obj;
 
-    if(x.isJSON === true) {
-      x.docs_json = JSON.parse(x.docs_json);
-    }
+    return {
+      renderValue: function(x) {
 
-    var refkey = Object.keys(x.docs_json)[0];
-    var refs = x.docs_json[refkey].roots.references;
+        if(x.isJSON === true) {
+          x.docs_json = JSON.parse(x.docs_json);
+        }
 
-    instance.modelid = x.modelid;
-    instance.elementid = x.elementid;
+        var refkey = Object.keys(x.docs_json)[0];
+        var refs = x.docs_json[refkey].roots.references;
 
-    if(x.debug === true) {
-      console.log('docid: ' + x.docid)
-      console.log('elementid: ' + x.elementid)
-      console.log('modelid: ' + x.modelid)
-      console.log(x.docs_json);
-      console.log(JSON.stringify(x.docs_json));
-    }
+        if(x.debug === true) {
+          console.log('docid: ' + x.docid)
+          console.log('elementid: ' + x.elementid)
+          console.log('modelid: ' + x.modelid)
+          console.log(x.docs_json);
+          console.log(JSON.stringify(x.docs_json));
+        }
 
-    // change 'nulls' in data to NaN
-    function traverseObject(obj) {
-      for(var key in obj) {
-        if(obj[key].constructor === Object) {
-          traverseObject(obj[key]);
-        } else if(obj[key].constructor === Array) {
-          for (var i = 0; i < obj[key].length; i++) {
-            if(obj[key][i] === null)
-              obj[key][i] = NaN;
+        // change 'nulls' in data to NaN
+        function traverseObject(obj) {
+          for(var key in obj) {
+            if(obj[key].constructor === Object) {
+              traverseObject(obj[key]);
+            } else if(obj[key].constructor === Array) {
+              for (var i = 0; i < obj[key].length; i++) {
+                if(obj[key][i] === null)
+                  obj[key][i] = NaN;
+              }
+            }
           }
         }
-      }
-    }
-    for(var i = 0; i < refs.length; i++) {
-      if(refs[i].type === 'ColumnDataSource')
-        traverseObject(refs[i].attributes.data);
-    }
 
-    var dv1 = document.createElement('div');
-    dv1.setAttribute('class', 'bk-root');
+        for(var i = 0; i < refs.length; i++) {
+          if(refs[i].type === 'ColumnDataSource')
+            traverseObject(refs[i].attributes.data);
+        }
 
-    var dv = document.createElement('div');
-    dv.id = x.elementid;
-    dv.setAttribute('class', 'plotdiv');
-    dv1.appendChild(dv);
-    el.appendChild(dv1);
+        var dv1 = document.createElement('div');
+        dv1.setAttribute('class', 'bk-root');
 
-    var render_items = [{
-      'docid': x.docid,
-      'elementid': x.elementid,
-      'modelid': x.modelid
-    }];
+        var dv = document.createElement('div');
 
-    if(x.debug !== true) {
-      Bokeh.set_log_level('info');
-    }
+        dv.id = x.elementid;
+        dv.setAttribute('class', 'plotdiv');
+        dv1.appendChild(dv);
+        el.appendChild(dv1);
 
-    Bokeh.embed.embed_items(x.docs_json, render_items);
-  },
+        var render_items = [{
+          'docid': x.docid,
+          'elementid': x.elementid,
+          'modelid': x.modelid
+        }];
 
-  resize: function(el, width, height, instance) {
+        if(x.debug !== true) {
+          Bokeh.set_log_level('info');
+        }
+
+        bokeh_obj = Bokeh.embed.embed_items(x.docs_json, render_items);
+      },
+
+      resize: function(width, height) {
+      },
+      obj: bokeh_obj
+    };
   }
 });
-
